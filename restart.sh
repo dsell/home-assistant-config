1d67c358d5610ff5e7f805448258d91f6c03a94e
Dennis <dsell2@sellfamily.us>
2019-06-29 17:46:37 -0500

    additional restart rework

--- restart.sh
+++ restart.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 curl -X POST "http://192.168.1.107:12101/api/text-to-speech" -H  "accept: text/plain" -H  "Content-Type: text/plain" -d  "Checking configuration"
-hassio homeassistant check
-if [ $@ != "ok" ] ; then
-curl -X POST "http://192.168.1.107:12101/api/text-to-speech" -H  "accept: text/plain" -H  "Content-Type: text/plain" -d  "Configuration Error detected.  Restart aborted."
+/usr/bin/hassio homeassistant check
+if [ $? -ne "0" ] ; then
+   curl -X POST "http://192.168.1.107:12101/api/text-to-speech" -H  "accept: text/plain" -H  "Content-Type: text/plain" -d  "Configuration Error detected.  Restart aborted."
 else
-curl -X POST "http://192.168.1.107:12101/api/text-to-speech" -H  "accept: text/plain" -H  "Content-Type: text/plain" -d  "Initiating ssytem restart."
-hassio homeassistant restart
+   curl -X POST "http://192.168.1.107:12101/api/text-to-speech" -H  "accept: text/plain" -H  "Content-Type: text/plain" -d  "Configuration acceptable.  Performing system restart."
+   /usr/bin/hassio homeassistant restart
 fi
